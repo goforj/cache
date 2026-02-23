430b0942ba7ac998c110f96562a8dd2494dd5ad3
Chris Miles <chris.miles.e@gmail.com>
2026-02-23 02:10:49 -0600

    ci: fix

--- example_compile_test.go
+++ example_compile_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -69,6 +70,10 @@ func buildExampleWithoutTags(exampleDir string) error {
 		return err
 	}
 
+	if err := os.WriteFile(filepath.Join(tmpDir, "go.mod"), []byte(exampleBuildGoMod()), 0644); err != nil {
+		return err
+	}
+
 	overlay := map[string]any{
 		"Replace": map[string]string{
 			abs(orig): abs(tmpFile),
@@ -87,10 +92,13 @@ func buildExampleWithoutTags(exampleDir string) error {
 
 	cmd := exec.Command(
 		"go", "build",
+		"-mod=mod",
 		"-overlay", overlayPath,
 		"-o", os.DevNull,
-		"./"+exampleDir,
+		".",
 	)
+	cmd.Dir = tmpDir
+	cmd.Env = append(os.Environ(), "GOWORK=off")
 
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
@@ -100,6 +108,47 @@ func buildExampleWithoutTags(exampleDir string) error {
 	}
 
 	return nil
+}
+
+func exampleBuildGoMod() string {
+	root := abs(".")
+	sep := string(filepath.Separator)
+	rootSlash := filepath.ToSlash(root)
+	if runtime.GOOS == "windows" {
+		// keep local replaces forward-slashed in go.mod for portability/parsing
+		rootSlash = strings.ReplaceAll(root, sep, "/")
+	}
+	lines := []string{
+		"module examplebuild",
+		"",
+		"go 1.24.4",
+		"",
+		"require (",
+		"\tgithub.com/goforj/cache v0.0.0",
+		"\tgithub.com/goforj/cache/cachecore v0.0.0",
+		"\tgithub.com/goforj/cache/driver/rediscache v0.0.0",
+		"\tgithub.com/goforj/cache/driver/memcachedcache v0.0.0",
+		"\tgithub.com/goforj/cache/driver/natscache v0.0.0",
+		"\tgithub.com/goforj/cache/driver/dynamocache v0.0.0",
+		"\tgithub.com/goforj/cache/driver/sqlcore v0.0.0",
+		"\tgithub.com/goforj/cache/driver/sqlitecache v0.0.0",
+		"\tgithub.com/goforj/cache/driver/postgrescache v0.0.0",
+		"\tgithub.com/goforj/cache/driver/mysqlcache v0.0.0",
+		")",
+		"",
+		"replace github.com/goforj/cache => " + rootSlash,
+		"replace github.com/goforj/cache/cachecore => " + rootSlash + "/cachecore",
+		"replace github.com/goforj/cache/driver/rediscache => " + rootSlash + "/driver/rediscache",
+		"replace github.com/goforj/cache/driver/memcachedcache => " + rootSlash + "/driver/memcachedcache",
+		"replace github.com/goforj/cache/driver/natscache => " + rootSlash + "/driver/natscache",
+		"replace github.com/goforj/cache/driver/dynamocache => " + rootSlash + "/driver/dynamocache",
+		"replace github.com/goforj/cache/driver/sqlcore => " + rootSlash + "/driver/sqlcore",
+		"replace github.com/goforj/cache/driver/sqlitecache => " + rootSlash + "/driver/sqlitecache",
+		"replace github.com/goforj/cache/driver/postgrescache => " + rootSlash + "/driver/postgrescache",
+		"replace github.com/goforj/cache/driver/mysqlcache => " + rootSlash + "/driver/mysqlcache",
+		"",
+	}
+	return strings.Join(lines, "\n")
 }
 
 func stripBuildTags(src []byte) []byte {
